1b605a5cd065b7a45e702e24a79c64ea3747cc6f
Guilherme Silva <guilherme.gss@outlook.com.br>
2021-06-20 19:53:38 -0300

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_19_224715) do
+ActiveRecord::Schema.define(version: 2021_06_20_203422) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
